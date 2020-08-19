f0a408eb91c8cca5ce99f6c28eefa05140cdeb4b
Taylor Allen <tjaroguetech@gmail.com>
2020-08-19 13:36:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array)
   i = 0
+  new_collection = []
   while i < array.length
-    yield(array[i])
+    new_collection = yield(array[i])
     i += 1
   end
+  new_collection
 end 
 
